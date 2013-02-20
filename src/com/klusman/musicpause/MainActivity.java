ff36462c4ce2f25d255db94bac320ea498fa1630
Shae <shae@fullsail.com>
2013-02-19 21:30:21 -0800

    List works and split Action Bar up and running

--- src/com/klusman/musicpause/MainActivity.java
+++ src/com/klusman/musicpause/MainActivity.java
@@ -11,19 +11,26 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import android.app.ListActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ListActivity {
-	File path = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_MUSIC);  //works
+	private File path = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_MUSIC);  //works
 	private ArrayList<String> songs = new ArrayList<String>();
-	Object mySongs[];
 	private String[] mySongsArray;
+	Button audio;
+	MediaPlayer mp = new MediaPlayer();
 
 
 	@Override
@@ -33,54 +40,91 @@ public class MainActivity extends ListActivity {
 		mySongsArray = songs.toArray(new String[songs.size()]);
 
 		setListAdapter(new myArrayAdapter(this, mySongsArray));
-
-		
-		
-		
-		
-		
-		
-		
+	
 	}  // END onCreate
 
+	
+	
+	
 	private void updatePlayList(){
 
 		Collection<File> files = FileUtils.listFiles(path,TrueFileFilter.INSTANCE,TrueFileFilter.INSTANCE);
     	if(files != null){
-
-    		mySongs = files.toArray();
-
     		
     		Iterator<File> itr = files.iterator(); 
     		while(itr.hasNext()) {
-
-    			String name = itr.next().getName();
-    			songs.add(name);
-    			
-    		    Log.i("SONG", name);
-    		    
-
-    		} 
-
-
-
+    			//String name = itr.next().getName(); // Works to show song title in tab
+    			String name = itr.next().getPath();  // works to show song path in tab
+    			songs.add(name);   			
+    		   // Log.i("SONG", name);
+    		} // End Iterator
     		
     		Log.i("# of Songs", String.valueOf(files.size()));
 
     	}else{
     		Log.i("Songs", "No Songs Found");
-    	}
+    	}  // End IF
    	
    }  // END UpdatePlayList
 	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-
-		//get selected items
+		if(mp.isPlaying()){
+			mp.stop();
+		}
+		
 		String selectedValue = (String) getListAdapter().getItem(position);
 		Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+		String newURI = selectedValue;
+		Uri uri;
+		try {
+			
+			uri = Uri.parse(newURI);
+			mp = MediaPlayer.create(MainActivity.this , uri);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(mp.isPlaying()){
+			mp.pause();
+		}else{
+			mp.start();
+			
+		}
+
 
 	}
+	
+	
 
 	
+////OPTIONS MENU  
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}// end onCreateOptionsMenu
+	
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()){
+
+			case R.id.menu_pause:
+					if(mp.isPlaying()){
+						mp.pause();
+
+					}else{
+						mp.start();
+
+					}
+				break;
+			case R.id.menu_stop:
+				mp.stop();
+				break;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
 }
